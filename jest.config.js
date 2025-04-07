f24c302e08f29cb17dfbcaf0f5906996b83e13e7
Ashmeet Sehgal <ashmeet.sehgal@walmart.com>
2025-04-08 02:44:14 +0530

    chore: update jest configuration to include moduleNameMapper for json-deep-compare

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,8 @@ module.exports = {
   },
   testEnvironment: "node",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "^json-deep-compare$": "<rootDir>/src"
+  }
 }
